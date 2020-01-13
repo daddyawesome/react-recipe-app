e9a2c6eed27b7974e651cafce00108953c0e1f78
daddyawesome <sablay296@gmail.com>
2020-01-13 17:16:01 -0500

    add app id and appkey

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+const App = () =>{
+
+  const APP_ID = "2fa07346";
+  const APP_KEY = "93d7e865c25e4663986f41023233bdfe";
+
+  const exampleReq =  `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
   return (
-    <div className="App">
-      <h1>Hello React</h1>
+    <div className = "App">
+      <form className="search-form">
+        <input className="search-bar" type="text"/>
+        <button className="search-button" type="submit">Search</button>
+      </form>
+
     </div>
   );
-}
+};
 
 export default App;
