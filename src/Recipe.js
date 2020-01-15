a029ff7d21eb1eb4b599688ad65bbb8c289fdb75
DaddyAwesome <sablay296@gmail.com>
2020-01-15 11:23:32 -0500

    change the place of img

--- src/Recipe.js
+++ src/Recipe.js
@@ -5,6 +5,7 @@ const Recipe =({title, calories, image, ingredients})=>{
     return(
         <div className={style.recipe}>
             <h1>{title}</h1>
+            <img className={style.image} src={image} alt=""/>
             <ol>
                 {ingredients.map(ingredient =>(
                 <li>
@@ -12,8 +13,8 @@ const Recipe =({title, calories, image, ingredients})=>{
                 </li>
                 ))}
             </ol>
-            <p>{calories}</p>
-            <img className={style.image} src={image} alt=""/>
+            <p>Calories - {calories}</p>
+            
         </div>
 
     );
